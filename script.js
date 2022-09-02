324c449570c26dcecead81986f99f435835d377e
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-09-02 15:03:56 +0200

    adding about card + Js listeners

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //shortcuts
 const aboutInfo = document.querySelector('.nav-about');
+const modalAbout = document.querySelector('.about-modal');
+
+//function
+const hideAboutCard = function (e) {
+  e.preventDefault();
+  if (modalAbout.classList)
+};
+
+//init
+modalAbout.classList.add('hidden');
 
 //Event listeners
+aboutInfo.addEventListener('click', function (e) {
+  e.preventDefault();
+  modalAbout.classList.toggle('hidden');
+
+});
